03f9da9cd0b7dde58150eacc39fe73f300ea5c75
Luiz Sócrate <luiz@socrate.com.br>
2013-02-21 18:35:55 -0300

    Create tables

--- Random-Tweet.php
+++ Random-Tweet.php
@@ -11,7 +11,23 @@ new RandomTweet($wpdb);
 class RandomTweet {
     private $db;
 
-    function __construct(wpdb $database) {
+    public function __construct(wpdb $database) {
         $this->db = $database;
+
+        add_action("init", array(&$this, "setup_plugin"));
+    }
+
+    public function setup_plugin() {
+        require_once(ABSPATH . 'wp-admin/includes/upgrade.php');
+
+        $this->db->random_tweet = $this->db->prefix . "random_tweet";
+        $sql = "CREATE TABLE {$this->db->random_tweet} (
+          id INT NOT NULL AUTO_INCREMENT,
+          technology VARCHAR(255) NOT NULL,
+          UNIQUE KEY id (id),
+          UNIQUE KEY technology (technology)
+        );";
+
+        dbDelta($sql);
     }
 }
